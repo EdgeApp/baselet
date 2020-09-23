e13b66ae98681996fb7e9e415f8acf6c72551654
Noah Passalacqua <noah@passabilities.dev>
2020-09-23 15:19:50 -0500

    Move RangeBase queryById functionality to separate find function.

--- src/RangeBase.ts
+++ src/RangeBase.ts
@@ -125,48 +125,49 @@ export function openRangeBase(
     }
 
     return openHashBase(disklet, configData.idDatabaseName).then(idDb => {
-      function getRanges(partition: string): Promise<[number, number]> {
-        const { rangeKey } = configData
+      /**
+       * Finds an item in the database partition for a given id.
+       * @param partition
+       * @param id
+       */
+      function find(partition: string, id: string): Promise<any> {
+        return idDb.query(partition, [id]).then(([range]) => {
+          if (range == null) return
 
-        const formattedPartition = checkAndformatPartition(partition)
-        const partitionPath = `${databaseName}${formattedPartition}`
-        return disklet.list(partitionPath).then(list => {
-          let minBucketNumber
-          let maxBucketNumber
-          for (const path in list) {
-            if (/config\.json$/.test(path)) continue
+          const { bucketSize, rangeKey, idKey } = configData
+          const formattedPartition = checkAndformatPartition(partition)
+          const bucketNumber = Math.floor(range / bucketSize)
+          const bucketFilename = `${bucketNumber}.json`
+          const bucketPath = `${databaseName}${formattedPartition}/${bucketFilename}`
 
-            const chucks = path.split('/')
-            const bucketNumber = Number(chucks[chucks.length - 1].split('.')[0])
-            if (minBucketNumber == null || bucketNumber < minBucketNumber)
-              minBucketNumber = bucketNumber
-            if (maxBucketNumber == null || bucketNumber > maxBucketNumber)
-              maxBucketNumber = bucketNumber
-          }
-
-          if (minBucketNumber == null) {
-            throw new Error(`no buckets found`)
-          }
-
-          const minPromise = disklet
-            .getText(`${partitionPath}/${minBucketNumber}.json`)
-            .then(rawBucketData => JSON.parse(rawBucketData))
-            .then(bucketData => {
-              return Number(bucketData[0][rangeKey])
+          return disklet
+            .getText(bucketPath)
+            .then((rawBucketData: string) => JSON.parse(rawBucketData))
+            .then((bucketData: any[]) => {
+              const firstRangeOccurence = getIndex(range, rangeKey, bucketData)
+              const lastRangeOccurence = getIndex(
+                range,
+                rangeKey,
+                bucketData,
+                firstRangeOccurence.index,
+                bucketData.length - 1,
+                true
+              )
+              const targetIndex = getIndex(
+                id,
+                idKey,
+                bucketData,
+                firstRangeOccurence.index,
+                lastRangeOccurence.index
+              )
+              if (targetIndex.found) {
+                return bucketData[targetIndex.index]
+              }
             })
-
-          const maxPromise = disklet
-            .getText(`${partitionPath}/${maxBucketNumber}.json`)
-            .then(rawBucketData => JSON.parse(rawBucketData))
-            .then(bucketData => {
-              return Number(bucketData[bucketData.length - 1][rangeKey])
-            })
-
-          return Promise.all([minPromise, maxPromise])
         })
       }
 
-      return {
+      const fns: RangeBase = {
         insert(partition: string, data: any): Promise<unknown> {
           const { bucketSize, rangeKey, idKey } = configData
           const formattedPartition = checkAndformatPartition(partition)
@@ -191,47 +192,50 @@ export function openRangeBase(
             const bucketFilename = `${bucketNumber}.json`
             const bucketPath = `${databaseName}${formattedPartition}/${bucketFilename}`
 
-            return disklet
-              .getText(bucketPath)
-              .then(
-                serializedBucket => {
-                  const bucketData = JSON.parse(serializedBucket)
-                  const firstRangeOccurence = getIndex(
-                    data[rangeKey],
-                    rangeKey,
-                    bucketData
-                  )
-                  if (firstRangeOccurence.found === false) {
-                    bucketData.splice(firstRangeOccurence.index, 0, data)
-                  } else {
-                    const lastRangeOccurence = getIndex(
+            return (
+              disklet
+                .getText(bucketPath)
+                .then(
+                  serializedBucket => {
+                    const bucketData: any[] = JSON.parse(serializedBucket)
+                    const firstRangeOccurence = getIndex(
                       data[rangeKey],
                       rangeKey,
-                      bucketData,
-                      firstRangeOccurence.index,
-                      bucketData.length - 1,
-                      true
+                      bucketData
                     )
-                    const targetIndex = getIndex(
-                      data[idKey],
-                      idKey,
-                      bucketData,
-                      firstRangeOccurence.index,
-                      lastRangeOccurence.index
+                    if (firstRangeOccurence.found === false) {
+                      bucketData.splice(firstRangeOccurence.index, 0, data)
+                    } else {
+                      const lastRangeOccurence = getIndex(
+                        data[rangeKey],
+                        rangeKey,
+                        bucketData,
+                        firstRangeOccurence.index,
+                        bucketData.length - 1,
+                        true
+                      )
+                      const targetIndex = getIndex(
+                        data[idKey],
+                        idKey,
+                        bucketData,
+                        firstRangeOccurence.index,
+                        lastRangeOccurence.index
+                      )
+                      bucketData.splice(targetIndex.index, 0, data)
+                    }
+                    return disklet.setText(
+                      bucketPath,
+                      JSON.stringify(bucketData)
                     )
-                    bucketData.splice(targetIndex.index, 0, data)
+                  },
+                  () => {
+                    // assuming bucket doesnt exist
+                    return disklet.setText(bucketPath, JSON.stringify([data]))
                   }
-                  return disklet.setText(bucketPath, JSON.stringify(bucketData))
-                },
-                () => {
-                  // assuming bucket doesnt exist
-                  return disklet.setText(bucketPath, JSON.stringify([data]))
-                }
-              )
-              .then(() => {
+                )
                 // Save the id
-                return idDb.insert(partition, data[idKey], data[rangeKey])
-              })
+                .then(() => idDb.insert(partition, data[idKey], data[rangeKey]))
+            )
           })
         },
         query(
@@ -243,142 +247,94 @@ export function openRangeBase(
           const formattedPartition = checkAndformatPartition(partition)
           const bucketFetchers: any[] = []
 
-          return getRanges(partition)
-            .then(([min, max]) => {
-              if (rangeStart < min) rangeStart = min
-              if (rangeEnd > max) rangeEnd = max
-            })
-            .then(() => {
-              rangeStart = Number(rangeStart)
-              rangeEnd = Number(rangeEnd)
-              if (rangeEnd < rangeStart) {
-                throw new Error(
-                  'range values must be integers and rangeEnd >= rangeStart'
-                )
-              }
+          if (rangeEnd < rangeStart) {
+            throw new Error('rangeStart must be larger than rangeEnd')
+          }
 
-              for (
-                let bucketNumber = Math.floor(rangeStart / bucketSize);
-                bucketNumber <= Math.floor(rangeEnd / bucketSize);
-                bucketNumber++
-              ) {
-                bucketFetchers.push(
-                  disklet
-                    .getText(
-                      `${databaseName}${formattedPartition}/${bucketNumber}.json`
-                    )
-                    .then(
-                      rawBucketData => JSON.parse(rawBucketData),
-                      () => []
-                    )
+          for (
+            let bucketNumber = Math.floor(rangeStart / bucketSize);
+            bucketNumber <= Math.floor(rangeEnd / bucketSize);
+            bucketNumber++
+          ) {
+            bucketFetchers.push(
+              disklet
+                .getText(
+                  `${databaseName}${formattedPartition}/${bucketNumber}.json`
                 )
-              }
+                .then(
+                  rawBucketData => JSON.parse(rawBucketData),
+                  () => []
+                )
+            )
+          }
 
-              return Promise.all(bucketFetchers).then(bucketList => {
-                let queryResults: any[] = []
-                if (bucketList.length === 1) {
-                  // only one bucket
+          return Promise.all(bucketFetchers).then(bucketList => {
+            let queryResults: any[] = []
+            if (bucketList.length === 1) {
+              // only one bucket
+              const firstRangeIndex = getIndex(
+                rangeStart,
+                rangeKey,
+                bucketList[0]
+              )
+              const lastRangeIndex = getIndex(
+                rangeEnd,
+                rangeKey,
+                bucketList[0],
+                0,
+                bucketList[0].length - 1,
+                true
+              )
+              queryResults = bucketList[0].slice(
+                firstRangeIndex.index,
+                lastRangeIndex.found
+                  ? lastRangeIndex.index + 1
+                  : lastRangeIndex.index
+              )
+            } else {
+              for (let i = 0; i < bucketList.length; i++) {
+                if (i === 0) {
+                  // is first bucket
                   const firstRangeIndex = getIndex(
                     rangeStart,
                     rangeKey,
-                    bucketList[0]
+                    bucketList[i]
                   )
+                  queryResults = bucketList[i].slice(firstRangeIndex.index)
+                } else if (i === bucketList.length - 1) {
+                  // is last bucket
                   const lastRangeIndex = getIndex(
                     rangeEnd,
                     rangeKey,
-                    bucketList[0],
+                    bucketList[i],
                     0,
-                    bucketList[0].length - 1,
+                    bucketList[i].length - 1,
                     true
                   )
-                  queryResults = bucketList[0].slice(
-                    firstRangeIndex.index,
-                    lastRangeIndex.found
-                      ? lastRangeIndex.index + 1
-                      : lastRangeIndex.index
+                  Array.prototype.push.apply(
+                    queryResults,
+                    bucketList[i].slice(
+                      0,
+                      lastRangeIndex.found
+                        ? lastRangeIndex.index + 1
+                        : lastRangeIndex.index
+                    )
                   )
                 } else {
-                  for (let i = 0; i < bucketList.length; i++) {
-                    if (i === 0) {
-                      // is first bucket
-                      const firstRangeIndex = getIndex(
-                        rangeStart,
-                        rangeKey,
-                        bucketList[i]
-                      )
-                      queryResults = bucketList[i].slice(firstRangeIndex.index)
-                    } else if (i === bucketList.length - 1) {
-                      // is last bucket
-                      const lastRangeIndex = getIndex(
-                        rangeEnd,
-                        rangeKey,
-                        bucketList[i],
-                        0,
-                        bucketList[i].length - 1,
-                        true
-                      )
-                      Array.prototype.push.apply(
-                        queryResults,
-                        bucketList[i].slice(
-                          0,
-                          lastRangeIndex.found
-                            ? lastRangeIndex.index + 1
-                            : lastRangeIndex.index
-                        )
-                      )
-                    } else {
-                      // is bucket in between range values
-                      queryResults.push.apply(queryResults, bucketList[i])
-                    }
-                  }
+                  // is bucket in between range values
+                  queryResults.push.apply(queryResults, bucketList[i])
                 }
-                return queryResults
-              })
-            })
-            .catch(() => [])
+              }
+            }
+            return queryResults
+          })
         },
         queryById(partition: string, id: string): Promise<any> {
-          return idDb.query(partition, [id]).then(([range]) => {
-            if (range == null) return
-
-            const { bucketSize, rangeKey, idKey } = configData
-            const formattedPartition = checkAndformatPartition(partition)
-            const bucketNumber = Math.floor(range / bucketSize)
-            const bucketFilename = `${bucketNumber}.json`
-            const bucketPath = `${databaseName}${formattedPartition}/${bucketFilename}`
-
-            return disklet
-              .getText(bucketPath)
-              .then(rawBucketData => JSON.parse(rawBucketData))
-              .then(bucketData => {
-                const firstRangeOccurence = getIndex(
-                  range,
-                  rangeKey,
-                  bucketData
-                )
-                const lastRangeOccurence = getIndex(
-                  range,
-                  rangeKey,
-                  bucketData,
-                  firstRangeOccurence.index,
-                  bucketData.length - 1,
-                  true
-                )
-                const targetIndex = getIndex(
-                  id,
-                  idKey,
-                  bucketData,
-                  firstRangeOccurence.index,
-                  lastRangeOccurence.index
-                )
-                if (targetIndex.found === false) {
-                  throw new Error(`Data with id ${id} not found.`)
-                }
-                return Promise.resolve(bucketData[targetIndex.index])
-              })
-          })
+          return find(partition, id)
         }
       }
+
+      return fns
     })
   })
 }
