9c1e3e35eb295d02bea85ba0d645a7aaf3030f98
Noah Passalacqua <noah@passabilities.dev>
2020-09-23 15:10:03 -0500

    Move HashBase query functionality to separate find function.

--- src/HashBase.ts
+++ src/HashBase.ts
@@ -47,7 +47,52 @@ export function openHashBase(
       throw new Error(`Tried to open HashBase, but type is ${configData.type}`)
     }
 
-    return {
+    function find(partition: string, hashes: string[]): Promise<any[]> {
+      const formattedPartition = checkAndformatPartition(partition)
+      if (hashes.length < 1) {
+        return Promise.reject(
+          new Error('At least one hash is required to query database.')
+        )
+      }
+      const { prefixSize } = configData
+      const bucketFetchers = []
+      const bucketDict: BucketDictionary = {}
+      for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
+        // TODO: check to make sure hash length is at least prefixSize
+        const bucketName: keyof typeof bucketDict = hashes[
+          inputIndex
+        ].substring(0, prefixSize)
+        if (bucketDict[bucketName] === undefined) {
+          const bucketFetcher = disklet
+            .getText(`${databaseName}${formattedPartition}/${bucketName}.json`)
+            .then(
+              serializedBucket => {
+                bucketDict[bucketName].bucketData = JSON.parse(serializedBucket)
+              },
+              () => {
+                // assume bucket doesn't exist
+              }
+            )
+          bucketDict[bucketName] = {
+            bucketFetcher,
+            bucketData: {}
+          }
+          bucketFetchers.push(bucketFetcher)
+        }
+      }
+      return Promise.all(bucketFetchers).then(() => {
+        const results = []
+        for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
+          const bucketName = hashes[inputIndex].substring(0, prefixSize)
+          const bucketData = bucketDict[bucketName].bucketData
+          const hashData = bucketData[hashes[inputIndex]]
+          results.push(hashData)
+        }
+        return results
+      })
+    }
+
+    const fns: HashBase = {
       insert(partition: string, hash: string, data: any): Promise<unknown> {
         const { prefixSize } = configData
         const formattedPartition = checkAndformatPartition(partition)
@@ -73,54 +118,11 @@ export function openHashBase(
         )
       },
       query(partition: string, hashes: string[]): Promise<any[]> {
-        const formattedPartition = checkAndformatPartition(partition)
-        if (hashes.length < 1) {
-          return Promise.reject(
-            new Error('At least one hash is required to query database.')
-          )
-        }
-        const { prefixSize } = configData
-        const bucketFetchers = []
-        const bucketDict: BucketDictionary = {}
-        for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
-          // TODO: check to make sure hash length is at least prefixSize
-          const bucketName: keyof typeof bucketDict = hashes[
-            inputIndex
-          ].substring(0, prefixSize)
-          if (bucketDict[bucketName] === undefined) {
-            const bucketFetcher = disklet
-              .getText(
-                `${databaseName}${formattedPartition}/${bucketName}.json`
-              )
-              .then(
-                serializedBucket => {
-                  bucketDict[bucketName].bucketData = JSON.parse(
-                    serializedBucket
-                  )
-                },
-                () => {
-                  // assume bucket doesn't exist
-                }
-              )
-            bucketDict[bucketName] = {
-              bucketFetcher,
-              bucketData: {}
-            }
-            bucketFetchers.push(bucketFetcher)
-          }
-        }
-        return Promise.all(bucketFetchers).then(() => {
-          const results = []
-          for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
-            const bucketName = hashes[inputIndex].substring(0, prefixSize)
-            const bucketData = bucketDict[bucketName].bucketData
-            const hashData = bucketData[hashes[inputIndex]]
-            results.push(hashData)
-          }
-          return results
-        })
+        return find(partition, hashes)
       }
     }
+
+    return fns
   })
 }
 
