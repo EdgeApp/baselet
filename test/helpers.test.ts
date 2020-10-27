33b1f07270e63e8f0630484020952f22d595fbfe
Noah Passalacqua <noah@passabilities.dev>
2020-10-27 12:59:59 -0500

    Add memlet and update functions.

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,20 +1,20 @@
 import { expect } from 'chai'
 
-import { checkAndformatPartition } from '../src/helpers'
+import { checkAndFormatPartition } from '../src/helpers'
 
 describe('checkAndformatPartition', function () {
   it('prefix partition with slash', function () {
     const partition = 'transactions'
-    expect(checkAndformatPartition(partition)).equals(`/${partition}`)
+    expect(checkAndFormatPartition(partition)).eql(partition)
   })
   it('fail if argument is not string', function () {
     const partition = 5
     expect(
-      checkAndformatPartition.bind(this, (partition as unknown) as string)
+      checkAndFormatPartition.bind(this, (partition as unknown) as string)
     ).to.throw(TypeError)
   })
   it('fail if partition contains invalid characters', function () {
     const partition = '/transactions'
-    expect(checkAndformatPartition.bind(this, partition)).to.throw(Error)
+    expect(checkAndFormatPartition.bind(this, partition)).to.throw(Error)
   })
 })
