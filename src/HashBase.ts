2de0b1359a1e194b6347fa4dbb979da0049c4e7b
Samuel Holmes <sam@edge.app>
2021-10-01 17:06:43 -0700

    Use async/await pattern in HashBase

--- src/HashBase.ts
+++ src/HashBase.ts
@@ -35,174 +35,174 @@ interface BucketDictionary {
   }
 }
 
-export function openHashBase(
+export async function openHashBase(
   disklet: Disklet,
   databaseName: string
 ): Promise<HashBase> {
   const memlet = getOrMakeMemlet(disklet)
 
-  return getConfig<HashBaseConfig>(disklet, databaseName).then(configData => {
-    if (configData.type !== BaseType.HashBase) {
-      throw new Error(`Tried to open HashBase, but type is ${configData.type}`)
+  const configData = await getConfig<HashBaseConfig>(disklet, databaseName)
+  if (configData.type !== BaseType.HashBase) {
+    throw new Error(`Tried to open HashBase, but type is ${configData.type}`)
+  }
+
+  async function find(
+    partition: string,
+    hashes: string[],
+    remove = false
+  ): Promise<any[]> {
+    if (hashes.length < 1) {
+      return Promise.reject(
+        new Error('At least one hash is required to query database.')
+      )
+    }
+    const { prefixSize } = configData
+    const bucketFetchers = []
+    const bucketDict: BucketDictionary = {}
+    for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
+      const hash = hashes[inputIndex]
+      if (hash.length < prefixSize) continue
+
+      const bucketName: keyof typeof bucketDict = hash.substring(0, prefixSize)
+      if (bucketDict[bucketName] === undefined) {
+        const bucketPath = getBucketPath(databaseName, partition, bucketName)
+        const bucketFetcher = memlet.getJson(bucketPath).then(
+          bucketData => (bucketDict[bucketName].bucketData = bucketData),
+          () => {
+            // assume bucket doesn't exist
+          }
+        )
+        bucketDict[bucketName] = {
+          bucketFetcher,
+          bucketPath,
+          bucketData: {}
+        }
+        bucketFetchers.push(bucketFetcher)
+      }
+    }
+    await Promise.all(bucketFetchers)
+
+    const results: any[] = []
+    const bucketNames = new Set<string>()
+    for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
+      const bucketName = hashes[inputIndex].substring(0, prefixSize)
+      bucketNames.add(bucketName)
+      const bucketData = bucketDict[bucketName].bucketData
+      const hashData = bucketData[hashes[inputIndex]]
+
+      if (remove) {
+        delete bucketData[hashes[inputIndex]]
+      }
+
+      results.push(hashData)
     }
 
-    function find(
-      partition: string,
-      hashes: string[],
-      remove = false
-    ): Promise<any[]> {
-      if (hashes.length < 1) {
-        return Promise.reject(
-          new Error('At least one hash is required to query database.')
-        )
-      }
-      const { prefixSize } = configData
-      const bucketFetchers = []
-      const bucketDict: BucketDictionary = {}
-      for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
-        const hash = hashes[inputIndex]
-        if (hash.length < prefixSize) continue
-
-        const bucketName: keyof typeof bucketDict = hash.substring(
-          0,
-          prefixSize
-        )
-        if (bucketDict[bucketName] === undefined) {
-          const bucketPath = getBucketPath(databaseName, partition, bucketName)
-          const bucketFetcher = memlet.getJson(bucketPath).then(
-            bucketData => (bucketDict[bucketName].bucketData = bucketData),
-            () => {
-              // assume bucket doesn't exist
-            }
-          )
-          bucketDict[bucketName] = {
-            bucketFetcher,
-            bucketPath,
-            bucketData: {}
-          }
-          bucketFetchers.push(bucketFetcher)
-        }
-      }
-      return Promise.all(bucketFetchers).then(() => {
-        const results: any[] = []
-        const bucketNames = new Set<string>()
-        for (let inputIndex = 0; inputIndex < hashes.length; inputIndex++) {
-          const bucketName = hashes[inputIndex].substring(0, prefixSize)
-          bucketNames.add(bucketName)
-          const bucketData = bucketDict[bucketName].bucketData
-          const hashData = bucketData[hashes[inputIndex]]
-
-          if (remove) {
-            delete bucketData[hashes[inputIndex]]
-          }
-
-          results.push(hashData)
-        }
-
-        let resultPromise: Promise<unknown> = Promise.resolve()
-        if (remove) {
-          const deletePromises = Array.from(bucketNames).map(bucketName => {
-            const { bucketPath, bucketData } = bucketDict[bucketName]
-            return memlet.setJson(bucketPath, bucketData)
-          })
-          resultPromise = Promise.all(deletePromises)
-        }
-
-        return resultPromise.then(() => results)
+    let resultPromise: Promise<unknown> = Promise.resolve()
+    if (remove) {
+      const deletePromises = Array.from(bucketNames).map(bucketName => {
+        const { bucketPath, bucketData } = bucketDict[bucketName]
+        return memlet.setJson(bucketPath, bucketData)
       })
+      resultPromise = Promise.all(deletePromises)
     }
 
-    const fns: HashBase = {
-      databaseName,
-      insert(partition: string, hash: string, data: any): Promise<unknown> {
-        const { prefixSize } = configData
-        if (hash.length < prefixSize) {
-          return Promise.reject(
-            new Error(`hash must be a string of length at least ${prefixSize}`)
-          )
-        }
+    await resultPromise
+    return results
+  }
 
-        const prefix = hash.substring(0, prefixSize)
-        const bucketPath = getBucketPath(databaseName, partition, prefix)
-        const setNewData = (oldData = {}): any =>
-          memlet.setJson(bucketPath, Object.assign(oldData, { [hash]: data }))
-        return memlet.getJson(bucketPath).then(
-          bucketData => setNewData(bucketData),
-          // assuming bucket doesnt exist
-          () => setNewData()
+  const out: HashBase = {
+    databaseName,
+
+    insert(partition: string, hash: string, data: any): Promise<unknown> {
+      const { prefixSize } = configData
+      if (hash.length < prefixSize) {
+        return Promise.reject(
+          new Error(`hash must be a string of length at least ${prefixSize}`)
         )
-      },
-      query(partition: string, hashes: string[]): Promise<any[]> {
-        return find(partition, hashes)
-      },
-      delete(partition: string, hashes: string[]): Promise<any[]> {
-        return find(partition, hashes, true)
-      },
-      dumpData(partition: string): Promise<any> {
-        const dump = (d: Disklet, data: any = {}): Promise<any> => {
-          return d.list().then(listing => {
-            return Promise.all(
-              Object.keys(listing).map(path => {
-                if (getConfigPath(databaseName).includes(path)) {
-                  return
-                }
+      }
 
-                const type = listing[path]
-                if (type === 'folder') {
-                  return dump(navigateDisklet(d, path), data[path]).then(
-                    folderData => {
-                      data[path] = folderData
-                    }
-                  )
-                }
-                if (type === 'file') {
-                  return d.getText(path).then(fileData => {
-                    data = { ...data, ...JSON.parse(fileData) }
-                  })
-                }
-              })
-            ).then(() => data)
-          })
-        }
+      const prefix = hash.substring(0, prefixSize)
+      const bucketPath = getBucketPath(databaseName, partition, prefix)
+      const setNewData = (oldData = {}): any =>
+        memlet.setJson(bucketPath, Object.assign(oldData, { [hash]: data }))
+      return memlet.getJson(bucketPath).then(
+        bucketData => setNewData(bucketData),
+        // assuming bucket doesnt exist
+        () => setNewData()
+      )
+    },
 
-        const partitionDisklet = navigateDisklet(
-          disklet,
-          getPartitionPath(databaseName, partition)
-        )
-        return dump(partitionDisklet).then(data => {
-          return {
-            config: configData,
-            data
-          }
+    query(partition: string, hashes: string[]): Promise<any[]> {
+      return find(partition, hashes)
+    },
+
+    delete(partition: string, hashes: string[]): Promise<any[]> {
+      return find(partition, hashes, true)
+    },
+
+    async dumpData(partition: string): Promise<any> {
+      const dump = (d: Disklet, data: any = {}): Promise<any> => {
+        return d.list().then(listing => {
+          return Promise.all(
+            Object.keys(listing).map(path => {
+              if (getConfigPath(databaseName).includes(path)) {
+                return
+              }
+
+              const type = listing[path]
+              if (type === 'folder') {
+                return dump(navigateDisklet(d, path), data[path]).then(
+                  folderData => {
+                    data[path] = folderData
+                  }
+                )
+              }
+              if (type === 'file') {
+                return d.getText(path).then(fileData => {
+                  data = { ...data, ...JSON.parse(fileData) }
+                })
+              }
+            })
+          ).then(() => data)
         })
       }
-    }
 
-    return fns
-  })
+      const partitionDisklet = navigateDisklet(
+        disklet,
+        getPartitionPath(databaseName, partition)
+      )
+      return dump(partitionDisklet).then(data => {
+        return {
+          config: configData,
+          data
+        }
+      })
+    }
+  }
+
+  return out
 }
 
-export function createHashBase(
+export async function createHashBase(
   disklet: Disklet,
   databaseName: string,
   prefixSize: number
 ): Promise<HashBase> {
-  databaseName = checkDatabaseName(databaseName)
+  const dbName = checkDatabaseName(databaseName)
   if (!isPositiveInteger(prefixSize)) {
     throw new Error(`prefixSize must be a number greater than 0`)
   }
 
-  return doesDatabaseExist(disklet, databaseName).then(databaseExists => {
-    if (databaseExists) {
-      throw new Error(`database ${databaseName} already exists`)
-    }
+  const databaseExists = await doesDatabaseExist(disklet, dbName)
+  if (databaseExists) {
+    throw new Error(`database ${dbName} already exists`)
+  }
 
-    const configData: HashBaseConfig = {
-      type: BaseType.HashBase,
-      prefixSize: prefixSize
-    }
-    return setConfig(disklet, databaseName, configData).then(() =>
-      openHashBase(disklet, databaseName)
-    )
-  })
+  const configData: HashBaseConfig = {
+    type: BaseType.HashBase,
+    prefixSize: prefixSize
+  }
+  await setConfig(disklet, dbName, configData)
+
+  return openHashBase(disklet, dbName)
 }
